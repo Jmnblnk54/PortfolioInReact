3dfc568cf97cb9de948ea789286e74ed37585a53
Joshua Blank <jmnblnk@yahoo.com>
2020-10-04 21:19:35 -0700

    Updates

---
+++ precache-manifest.604b7bd40fa2bd2cfc3ffe8e352a840a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e1779102bafe9ef2e605b45caa2938",
+    "url": "/PortfolioInReact/index.html"
+  },
+  {
+    "revision": "135bec597390a8d5a055",
+    "url": "/PortfolioInReact/static/css/main.c3c0f040.chunk.css"
+  },
+  {
+    "revision": "c53eaf5849081ccdb9c2",
+    "url": "/PortfolioInReact/static/js/2.2c40fc07.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/PortfolioInReact/static/js/2.2c40fc07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135bec597390a8d5a055",
+    "url": "/PortfolioInReact/static/js/main.825627e9.chunk.js"
+  },
+  {
+    "revision": "067e9a527594e6b418c2",
+    "url": "/PortfolioInReact/static/js/runtime-main.cf717069.js"
+  }
+]);
